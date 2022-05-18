05e6f23632359d996bacea544f3c615f5b9d0ae6
Jerry C <jerry.comerasamy@homunity.fr>
2022-05-18 16:16:40 +0200

    Buy transaction when conditions are met

--- src/trade.rs
+++ src/trade.rs
@@ -41,6 +41,22 @@ pub mod trade {
         currency: String,
     }
 
+    #[derive(Deserialize, Debug)]
+    pub struct Account {
+        pub id: String,
+        pub name: String,
+        pub balance: AccountBalance,
+    }
+    #[derive(Deserialize, Debug)]
+    pub struct AccountBalance {
+        pub amount: String,
+        pub currency: String,
+    }
+    #[derive(Deserialize, Debug)]
+    pub struct DataAccount {
+        pub data: Account,
+    }
+
     fn connect(method: &str, path: &str, body: &str) -> reqwest::blocking::RequestBuilder {
         let now = SystemTime::now();
 
@@ -79,29 +95,50 @@ pub mod trade {
         println!("=======================================================================");
     }
 
-    fn buy (currency: &str) {
+    fn buy(currency: &str, account_euro: &Account) -> Result<(), String> {
+        let buy_amount = env::var("buy_amount").expect("");
+        let min_eur = env::var("min_eur").expect("");
+
+        let balance: f64 = account_euro.balance.amount.parse::<f64>().unwrap();
+        let buy_amount_f: f64 = buy_amount.parse::<f64>().unwrap();
+        let min_eur_f: f64 = min_eur.parse::<f64>().unwrap();
+
+        if (balance - buy_amount_f) < min_eur_f {
+            println!("Wallet amount will be less than what you authorized : {}", balance);
+            return Ok(());
+        }
+
+        let last_sell_price = database::get_last_price(&currency.to_string(), &"BUY_AT");
+        let amount = buy_amount_f / last_sell_price.value;
+
+
         let buy_parameter: BuyParameters = BuyParameters {
-            amount: 10.00,
+            amount: amount,
             currency: currency.to_string(),
         };
         let buy_parameter_json = serde_json::to_string(&buy_parameter).expect("Expected Json as String");
 
         println!("{:?}", buy_parameter_json);
 
-        //let id: String = get_account_id();
-        let url: String = format!("/v2/accounts/{}/buys", "97cee244-d348-5001-81f1-87b98090fb76");
+        let account_parameter = "account_id_".to_owned() + &currency.to_lowercase();
+        let account_id = env::var(account_parameter).expect(
+            &format!("Check your account_id{} in .env file", &currency)
+        );
+
+        let url: String = format!("/v2/accounts/{}/buys", account_id);
 
         let res = connect("POST", &url, &buy_parameter_json);
-
         let response = match res.body(buy_parameter_json).send() {
             Ok(r) => r.text(),//::<Response>(),
             Err(_) => panic!("Json error")
         };
 
-        println!("Welcome {:?}", response);
+        Ok(())
     }
 
     pub fn should_we_buy() {
+        let account_euro_id = env::var("account_id_eur").expect("Check your api_key in .env file");
+        let account_euro = get_account(&account_euro_id);
 
         let cryptos = get_cryptos();
         for crypto in cryptos {
@@ -117,7 +154,7 @@ pub mod trade {
             let dt = Utc::now() +- Duration::days(check_period.parse::<i64>().expect("check_period must be an integer"));
 
             let timestamp = database::last_sell_prices(&dt.format("%F").to_string(), &crypto);
-            let actual_price = database::get_last_sell_price(&crypto);
+            let actual_price = database::get_last_price(&crypto, &"BUY_AT");
 
             for value in timestamp {
                 let compare_price = ((percentage / 100.0)) * value.value;
@@ -132,7 +169,7 @@ pub mod trade {
                     println!("Compare price : {:?}", compare_price);
 
                     // We should Buy
-                    buy(&crypto);
+                    buy(&crypto, &account_euro);
                     break;
                 }
             }
@@ -180,74 +217,15 @@ pub mod trade {
         headers
     }
 
-    pub fn get_id () -> String {
-        let res = connect("GET", "/v2/user", "");
+    pub fn get_account(account_id: &str) -> Account {
+        let path = format!("/v2/accounts/{}", account_id);
+        let res = connect("GET", &path, "");
 
         let response = match res.send() {
-            Ok(r) => r.json::<Response>(),
-            Err(_) => panic!("Json error")
-        };
-println!("{:?}", response);
-        response.expect("test").data.id
-    }
-
-    pub fn get_account_id () -> String {
-        #[derive(Deserialize, Debug)]
-        struct Account {
-            id: String,
-            name: String,
-            balance: AccountBalance,
-        }
-        #[derive(Deserialize, Debug)]
-        struct AccountBalance {
-            amount: String,
-            currency: String,
-        }
-        #[derive(Deserialize, Debug)]
-        struct PaginationAccount {
-            next_uri: String,
-        }
-        #[derive(Deserialize, Debug)]
-        struct DataAccount {
-            data: Vec<Account>,
-            pagination: PaginationAccount
-        }
-
-        let res = connect("GET", "/v2/accounts", "");
-
-        let mut response = match res.send() {
             Ok(r) => r.json::<DataAccount>(),
             Err(_) => panic!("Json error")
         };
 
-        println!("{:?}", &response);
-
-        for mut i in 0..10 {
-            let next:&str = match &response.as_ref() {
-                Ok(n) => &n.pagination.next_uri,
-                Err(_) => continue,
-            };
-
-            // let next: &str = &response.as_ref().expect("Test 1").pagination.next_uri;
-            println!("{:?}", next);
-            let res = connect("GET", next, "");
-
-            response = match res.send() {
-                Ok(r) => r.json::<DataAccount>(),
-                Err(_) => panic!("Json error")
-            };
-
-            println!("{:?}", response);
-
-            // for data in &response.as_ref().expect("2").data {
-            //     println!("{:?}", data);
-            // }
-            i += 1;
-        }
-
-//println!("{:?}", response);
-
-"Test".to_string()
-        //response.expect("test").data.id
+        response.expect("Account is not readable").data
     }
 }
